37b32b6f93fe125a3d56fee5aa084b2ed1276171
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-11-16 22:15:07 -0800

    fixes review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -15,35 +15,38 @@ const indexReviews = (req, res) => {
 };
 
 const createReview = (req, res) => {
-    console.log(req.body)
-    console.log(1)
-    const review = { author: req.session.currentUser.id, author_name: req.session.currentUser.name, review_text: req.body.review_text  }
+    const review =
+        {
+            author: req.session.currentUser.id,
+            author_name: req.session.currentUser.name,
+            review_text: req.body.review_text
+        }
+    if (!currentUser.name) return res.status(401).json({
+        status: 401,
+        message: 'Must be logged in as a client to write reviews.'
+    })
 
     db.Review.create(review, (err, createdReview) => {
         if (err) return res.status(500).json({
             status: 500,
             message: 'Something went wrong, please try again.'
         });
-        console.log(2)
-
 
         db.Attorney.findById(req.body.attorney_id, (err, foundAttorney) => {
             if (err) return res.status(500).json({
                 status: 500,
-                message: 'Something wwent wrong, please try again.'
+                message: 'Something went wrong, please try again.'
             });
 
             foundAttorney.reviews.push(createdReview._id);
             foundAttorney.save();
         });
-        console.log(3)
 
         db.Client.findById(req.session.currentUser.id, (err, foundClient) => {
             if (err) return res.status(500).json({
                 status: 500,
-                message: 'Something went wrrong, please try again.'
+                message: 'Something went wrong, please try again.'
             });
-            console.log(5)
 
             foundClient.reviews.push(createdReview._id);
             foundClient.save();
@@ -60,7 +63,7 @@ const editReview = (req, res) => {
     db.Review.findByIdAndUpdate(req.params.id, req.body, (err, editedReview) => {
         if (err) return res.status(500).json({
             status: 500,
-            message: 'Something went wrong, please try again.'
+            message: 'Something went wrong, please try again. 4'
         });
 
         res.status(202).json({
