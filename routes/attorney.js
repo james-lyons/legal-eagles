5b3de24386b142dac60bd915ee6f2dae046b3881
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2019-11-16 21:53:44 -0800

    auth edits

--- routes/attorney.js
+++ routes/attorney.js
@@ -4,8 +4,8 @@ const ctrl = require('../controllers');
 const authRequired = require('../middleware/authRequired');
 
 // Attorney Routes
-router.get('/search', authRequired, ctrl.attorney.indexAttorneys);
-router.get('/search/id/:id', authRequired, ctrl.attorney.showAttorneyById);
+router.get('/search', ctrl.attorney.indexAttorneys);
+router.get('/search/id/:id', ctrl.attorney.showAttorneyById);
 router.get('/search/url/:url', authRequired, ctrl.attorney.showAttorneyByURL);
 router.put('/:id', authRequired, ctrl.attorney.editAccount);
 router.delete('/:id', authRequired, ctrl.attorney.deleteAccount);
